f2699d3b0f58a77a5d8b948c1a3cb2720c8451a7
Tcubstead <tec44912@email.vccs.edu>
2024-10-01 17:04:44 -0400

    created user promts to collect infromation about the principal amount, interest rate, and amount of compounds

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -3,11 +3,33 @@
 // Chapter 3 Project
 // 3_Interest_Earned
 
+// This program calculates the amount of money that would be present after an amount of time of compounded interest in a savings account
 #include <iostream>
+#include <iomanip>
+#include <iostream>
+#include <cmath>  
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    // Variables to hold user input
+    double principal;
+    double Irate;
+    int timesCompounded;
+    double totInterest;
+
+    // user input of principal amount
+    cout << "Enter the principal amount (initial investment): ";
+    cin >> principal;
+
+    // user input of interest rate as a percentage
+    cout << "Enter the annual interest rate (as a percentage): ";
+    cin >> Irate;
+
+    // user input of amount of times compounded 
+    cout << "Enter the number of times interest is compounded per year: ";
+    cin >> timesCompounded;
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
